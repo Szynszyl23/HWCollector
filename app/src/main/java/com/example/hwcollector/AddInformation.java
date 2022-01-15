6f444bc1fdd9924a87334f906ccb673b18db35c4
DESKTOP-2HE7VG7\USER <szymon299@op.pl>
2022-01-15 12:46:35 +0100

    Created AddInformation activity that will be responsible for adding information to database. Layout is incomplete.

---
+++ app/src/main/java/com/example/hwcollector/AddInformation.java
@@ -0,0 +1,14 @@
+package com.example.hwcollector;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_information);
+    }
+}
