17373a84899ec8d7f9588c7b5379754e3d5965cf
DESKTOP-2HE7VG7\USER <szymon299@op.pl>
2022-01-15 11:48:57 +0100

    Added code to MainActivity.java that changes this activity to ListView activity after 2 seconds after turning on app. Changed .DarkActionBar to .NoActionBar in themes.xml to get rid of action bar on top of screen.

--- app/src/main/java/com/example/hwcollector/MainActivity.java
+++ app/src/main/java/com/example/hwcollector/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.hwcollector;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//This part changes app's MainActivity to ListView activity after 2 seconds
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, ListView.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
     }
 }
