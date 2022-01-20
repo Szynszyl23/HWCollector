71697906fa1d0bdb84b2cbbff4e11be4d25281cd
DESKTOP-2HE7VG7\USER <szymon299@op.pl>
2022-01-20 14:18:07 +0100

    Expanden layout in AddInformation activity. User can't save his progrees when at least one of EditTexts is empty to prevent storing empty cells in database.

--- app/src/main/java/com/example/hwcollector/AddInformation.java
+++ app/src/main/java/com/example/hwcollector/AddInformation.java
@@ -9,10 +9,12 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Calendar;
 
@@ -20,11 +22,12 @@ import yuku.ambilwarna.AmbilWarnaDialog;
 
 public class AddInformation extends AppCompatActivity {
 
-   TextView letterCounter, letterCounter2;
-   EditText addName, addWheelType;
-   Spinner yearSpinner;
+   TextView letterCounter, letterCounter2, letterCounter3;
+   EditText addName, addWheelType, addSeriesName;
+   Spinner yearSpinner, seriesNumberSpinner, seriesTypeSpinner;
    ImageButton backButton, mainColorButton, secondColorButton, thirdColorButton, tireColorButton, wheelsColorButton, rimColorButton;
-   String[] arrayList;
+   Button finishAndSave;
+   String[] arrayYearList, arraySeriesList, arraySeriesTypeList;
    int mainDefaultColor, secondDefaultColor, thirdDefaultColor, tireDefaultColor, wheelDefaultColor, rimDefaultColor;
 
     @Override
@@ -33,8 +36,10 @@ public class AddInformation extends AppCompatActivity {
         setContentView(R.layout.activity_add_information);
         GetComponents();
         LetterCounters();
-        CreateList();
-        CreateArrayAdapter();
+        CreateArrayLists();
+        CreateYearArrayAdapter();
+        CreateSeriesArrayAdapter();
+        CreateSeriesTypeArrayAdapter();
         AssignColors();
         ButtonListeners();
     }
@@ -57,14 +62,19 @@ public class AddInformation extends AppCompatActivity {
         tireColorButton.setOnClickListener(view -> ShowTireColorPicker());
         wheelsColorButton.setOnClickListener(view -> ShowWheelColorPicker());
         rimColorButton.setOnClickListener(view -> ShowRimColorPicker());
+        finishAndSave.setOnClickListener(view -> FinishAndSave());
     }
 
     private void GetComponents() {
         letterCounter = findViewById(R.id.lettersCounter);
         letterCounter2 = findViewById(R.id.lettersCounter2);
+        letterCounter3 = findViewById(R.id.lettersCounter3);
         addName = findViewById(R.id.nameEditText);
         addWheelType = findViewById(R.id.wheelTypeEditText);
+        addSeriesName = findViewById(R.id.seriesNameEditText);
         yearSpinner = findViewById(R.id.yearSpinner);
+        seriesNumberSpinner = findViewById(R.id.seriesNumberSpinner);
+        seriesTypeSpinner = findViewById(R.id.seriesTypeSpinner);
         backButton = findViewById(R.id.backBT);
         mainColorButton= findViewById(R.id.mainColor);
         secondColorButton= findViewById(R.id.secondColor);
@@ -72,6 +82,7 @@ public class AddInformation extends AppCompatActivity {
         tireColorButton= findViewById(R.id.tireColor);
         wheelsColorButton= findViewById(R.id.wheelColor);
         rimColorButton= findViewById(R.id.rimColor);
+        finishAndSave = findViewById(R.id.finishBT);
     }
 
     //This part of code allows to show dialog window with color picker in it, after accepting, color of assigned ImageButton or Button changes to one chosen in color picker
@@ -186,24 +197,41 @@ public class AddInformation extends AppCompatActivity {
     private void LetterCounters() {
         letterCounter.setText("50/50");
         letterCounter2.setText("50/50");
+        letterCounter3.setText("50/50");
         addName.addTextChangedListener(editTextWatcherForName);
         addWheelType.addTextChangedListener(editTextWatcherForWheelType);
+        addSeriesName.addTextChangedListener(editTextWatcherForSeriesName);
     }
 
     //This function creates ArrayAdapter that allows to populate assigned Spinner with records from arrayList
-    private void CreateArrayAdapter() {
+    private void CreateYearArrayAdapter() {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_spinner_item, arrayList);
+                android.R.layout.simple_spinner_item, arrayYearList);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         yearSpinner.setAdapter(adapter);
+    }
+
+    //This function creates ArrayAdapter that allows to populate assigned Spinner with records from arrayList
+    private void CreateSeriesArrayAdapter() {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, arraySeriesList);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        seriesNumberSpinner.setAdapter(adapter);
+    }
+
+    private void CreateSeriesTypeArrayAdapter() {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, arraySeriesTypeList);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        seriesTypeSpinner.setAdapter(adapter);
     }
 
     //This function allows to go back to previous activity after user's confirmation
     public void alertDialog()
     {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage("Closing")
-                .setTitle("Are you sure you want to exit without saving?")
+        builder.setMessage("Are you sure you want to exit without saving?")
+                .setTitle("Closing")
                 .setPositiveButton("Yes", (dialog, id) -> {
                     Intent intent = new Intent(AddInformation.this, ListView.class);
                     startActivity(intent);
@@ -215,7 +243,7 @@ public class AddInformation extends AppCompatActivity {
         builder.create().show();
     }
 
-    // This part of code enables letter counting from assigned EditText and displaying results in assigned TextView to show user how many character can be still used
+    //This part of code enables letter counting from assigned EditText and displaying results in assigned TextView to show user how many character can be still used
     private final TextWatcher editTextWatcherForName = new TextWatcher() {
         @Override
         public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -233,7 +261,7 @@ public class AddInformation extends AppCompatActivity {
         }
     };
 
-    // This part of code enables letter counting from assigned EditText and displaying results in assigned TextView to show user how many character can be still used
+    //This part of code enables letter counting from assigned EditText and displaying results in assigned TextView to show user how many character can be still used
     private final TextWatcher editTextWatcherForWheelType = new TextWatcher() {
         @Override
         public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -251,17 +279,74 @@ public class AddInformation extends AppCompatActivity {
         }
     };
 
-    // This function creates list of strings and populate it with years from 1968 to the current one
-    private void CreateList()
+    //This part of code enables letter counting from assigned EditText and displaying results in assigned TextView to show user how many character can be still used
+    private final TextWatcher editTextWatcherForSeriesName = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            letterCounter3.setText(50 - editable.toString().length() + "/50");
+        }
+    };
+
+    //This function creates list of strings and populate it with years from 1968 to the current one
+    //This function creates list of strings and populate it with numbers from 1 to 250
+    //This function creates list of strings and populate it with Series types
+    private void CreateArrayLists()
     {
         int thisYear = Calendar.getInstance().get(Calendar.YEAR);
         int howManyYears = thisYear - 1967;
         int actualYear = 1968;
-        arrayList = new String[howManyYears];
+        arraySeriesList = new String[250];
+        arrayYearList = new String[howManyYears];
+        arraySeriesTypeList = new String[5];
+        arraySeriesTypeList[0] = "Mainline";
+        arraySeriesTypeList[1] = "Special";
+        arraySeriesTypeList[2] = "TH";
+        arraySeriesTypeList[3] = "STH";
+        arraySeriesTypeList[4] = "RLC";
             for( int i = 0; i < howManyYears; i++)
             {
-                arrayList[i] = String.valueOf(actualYear + i);
+                arrayYearList[i] = String.valueOf(actualYear + i);
+            }
+            for( int i = 0; i<250; i++)
+            {
+                arraySeriesList[i] = String.valueOf(i + 1);
             }
         }
+
+    //This part of code brings user back to previous activity and shows dialog confirming adding record to database (no database for now)
+    //User can't save information while any of EditTexts is empty
+    private void FinishAndSave()
+    {
+        if(addName.length() > 0)
+        {
+            if(addWheelType.length() > 0)
+            {
+                if (addSeriesName.length() > 0)
+                {
+                    Intent intent = new Intent(AddInformation.this, ListView.class);
+                    startActivity(intent);
+                    try {
+                        Toast.makeText(AddInformation.this, "Added model: " + addName.getText().toString(), Toast.LENGTH_SHORT).show();
+                    } catch (Exception e) {
+                        Toast.makeText(AddInformation.this, "Model couldn't be added", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+                else {addSeriesName.setError("Space empty"); Toast.makeText(AddInformation.this, "Series name is empty", Toast.LENGTH_SHORT).show();}
+            }
+            else {addWheelType.setError("Space empty"); Toast.makeText(AddInformation.this, "Wheel type is empty", Toast.LENGTH_SHORT).show();}
+        }
+        else {addName.setError("Space empty"); Toast.makeText(AddInformation.this, "Model name is empty", Toast.LENGTH_SHORT).show();}
+    }
     }
 
